2718bfef9a692168197877771bf31e2a7e4c1028
Ondrej Sika <ondrej@ondrejsika.com>
2022-02-16 07:42:59 +0100

    feat(cmd/kubernetes/repo_setup): Add command for setup of ondrejsika/kubernetes-training repo

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/ondrejsika/training-cli/cmd/kubernetes"
 	_ "github.com/ondrejsika/training-cli/cmd/kubernetes/connect"
 	_ "github.com/ondrejsika/training-cli/cmd/kubernetes/install"
+	_ "github.com/ondrejsika/training-cli/cmd/kubernetes/repo_setup"
 	"github.com/ondrejsika/training-cli/cmd/root"
 	_ "github.com/ondrejsika/training-cli/cmd/version"
 	"github.com/spf13/cobra"
