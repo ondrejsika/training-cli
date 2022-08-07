795d2379f2b03cafd92c0b940a3e297e67611c40
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-07 17:45:55 +0200

    feat(cmd/rancher/vm_setup): Create rancher VM setup command

--- utils/rancher_utils/rancher_utils.go
+++ utils/rancher_utils/rancher_utils.go
@@ -5,6 +5,11 @@ import "github.com/ondrejsika/training-cli/utils/script_utils"
 func VMSetup() {
 	sh("mkdir -p .rancher-training-bin")
 	sh("slu install-bin-tool --bin-dir .rancher-training-bin rancher")
+
+	file(".bashrc.rancher-training", `# rancher-training bashrc
+export PATH="$PATH":$HOME/.rancher-training-bin
+`)
+	sh(`echo ". ~/.bashrc.rancher-training\n" >> .bashrc`)
 }
 
 // utils
