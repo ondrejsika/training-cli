3208b6667dcf86a2df8ef946a1ace4517384e7ac
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-06 06:26:21 +0100

    feat(utils/kafka_utils): Add install JDK & Kafka binaries

--- utils/kafka_utils/kafka_utils.go
+++ utils/kafka_utils/kafka_utils.go
@@ -6,8 +6,17 @@ func VMSetup() {
 	sh("mkdir -p .training/kafka-bin")
 	sh("slu install-bin --bin-dir .training/kafka-bin kaf")
 
+	sh("wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -")
+	sh("sudo add-apt-repository 'deb https://apt.corretto.aws stable main'")
+	sh("sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk")
+
+	sh("wget https://archive.apache.org/dist/kafka/3.0.0/kafka_2.13-3.0.0.tgz")
+	sh("tar -xzf kafka_2.13-3.0.0.tgz")
+	sh("mv kafka_2.13-3.0.0 .training/kafka_2.13-3.0.0")
+
 	file(".bashrc.kafka-training", `# kafka-training bashrc
 export PATH="$PATH":$HOME/.training/kafka-bin
+export PATH="$PATH:~/.training/kafka_2.13-3.0.0/bin"
 
 source <(kaf completion bash)
 `)
